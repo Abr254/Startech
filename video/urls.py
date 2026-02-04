88f68d82c5a95e25413395435fbcc1b9ee2a4bd0
Bkbrian <155978527+Abr254@users.noreply.github.com>
2026-02-04 13:31:24 +0300

    Update urls.py

--- video/urls.py
+++ video/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import video_list
-from regis.views import group_chat
+#from regis.views import group_chat
 urlpatterns = [
     path('', video_list, name='video_list'),  # This will map the root URL of the app to the video_list view
 ]
