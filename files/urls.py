640c127c7054eb52553d4d41d7771e05970b44f9
Bkbrian <155978527+Abr254@users.noreply.github.com>
2026-02-04 13:17:48 +0300

    Update urls.py

--- files/urls.py
+++ files/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from files.views import upload_file, download_file, list_files
+from regis.views import group_chat
 
 urlpatterns = [
     path('upload/', upload_file, name='upload_file'),
